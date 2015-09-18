d42d8edac6613139645b127bc50ba097fd3e880a
Matthew S. MacLennan <msmaclennan@gmail.com>
2015-09-18 12:53:22 -0700

    Update NOC2006.R

--- NOC2006.R
+++ NOC2006.R
@@ -1,3 +1,11 @@
+NOC2006list<-readHTMLList(NOC2006)[c(5:180)]
+biglist<-unique(unlist(lapply(readHTMLList(NOC2006)[c(5:180)],function(x) strsplit(x," *\\\r\\\n *",perl=T))))
+threedigit<-unlist(regmatches(biglist,gregexpr("^[0-9]{3}\\s.*",biglist,perl=T)))
+fourdigit<-unlist(regmatches(biglist,gregexpr("^[0-9]{4}\\s.*",biglist,perl=T)))
+fourdigitmatrix<-matrix(unlist(strsplit(fourdigit,"(?<=[0-9]{4})\\s",perl=T)),ncol=2,byrow=T)
+
+##############################################
+#Playing around...
 NOC2006<-htmlParse(getURL("http://www30.hrsdc.gc.ca/NOC/English/NOC/2006/QuickSearch.aspx?val65=*"),useInternalNode=T)
 NOC2006classes.h2<-unlist(xpathApply(NOC2006,"//h2",xmlValue))
 onedigit<-matrix(unlist(strsplit(NOC2006classes.h2,"(?<=[0-9]{1})",perl=T)),ncol=2,byrow=T)[3:12,]
@@ -9,3 +17,7 @@ NOC2006classes.h5<-unlist(xpathApply(NOC2006,"//h5",xmlValue))
 threedigit<-matrix(unlist(strsplit(NOC2006classes.h5,"(?<=[0-9 ]{3})",perl=T)),ncol=2,byrow=T)
 threedigitcodematrix<-matrix(as.numeric(unlist(strsplit(threedigit[,1],""))),ncol=3,byrow=T)
 onematrix<-
+
+
+
+threedigit[unlist(gregexpr("^0",threedigit[,1]))>0,1]
